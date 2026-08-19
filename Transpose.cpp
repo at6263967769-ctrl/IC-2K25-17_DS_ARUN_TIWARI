fa85e1cf13054978a8239f40f822a76accbe70f3
at6263967769-ctrl <at6263967769@gmail.com>
2026-08-19 12:27:37 +0530

    Implement 3x3 matrix transpose functionality
    This program takes a 3x3 matrix as input and outputs its transpose.

---
+++ Transpose.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int arr[3][3];
+    cout<<"Enter 2D elemetnts:"<<endl;
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            cin>>arr[i][j];
+            
+        }
+    }
+    cout<<"Transpose are:"<<endl;
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            cout<<arr[j][i]<<" ";
+        }
+    }
+
+    return 0;
+}
